2b4febb8e8936c96db0a301f484d5590d779b25f
Mithun Dhanasuthan <85911312+mithustar39@users.noreply.github.com>
2024-08-23 21:04:34 -0400

    Update seo.js

--- src/data/seo.js
+++ src/data/seo.js
@@ -2,14 +2,14 @@ const SEO = [
 	{
 		page: "home",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
+			"I am an engineering enthusiast with expertise in Java, JavaScript, CSS, HTML, React, Firebase, Python. ",
 		keywords: ["Mithun", "Mithun D", "Mithun Dhanasthan"],
 	},
-
+//Was in the above description, also was in the below one: I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.
 	{
 		page: "about",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
+			"I am an engineering enthusiast with expertise in Java, JavaScript, CSS, HTML, React, Firebase, Python.",
 		keywords: ["Mithun", "Mithun D", "Mithun Dhanasthan"],
 	},
 
